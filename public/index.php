5ba977a95d8bc23084adff27595344325e5ea9b2
Maoz <maozx2@gmail.com>
2015-04-30 14:56:13 +0200

    test

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 
 // Setup autoloading
 require 'init_autoloader.php';
-
+echo "sdsd";
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();

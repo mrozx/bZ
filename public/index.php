9ce6d099c35c73403220464c964393a601a52add
Maoz <maozx2@gmail.com>
2015-05-01 10:45:41 +0200

    today

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ chdir(dirname(__DIR__));
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
-echo "222";
 // Setup autoloading
 require 'init_autoloader.php';
 

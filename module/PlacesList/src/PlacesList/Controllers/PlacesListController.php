9001044722e63b09e515d3a5d379546a837d38bc
Maoz <maozx2@gmail.com>
2015-05-01 18:57:12 +0200

    withPlaceList01

--- module/PlacesList/src/PlacesList/Controllers/PlacesListController.php
+++ module/PlacesList/src/PlacesList/Controllers/PlacesListController.php
@@ -3,25 +3,10 @@
 namespace PlacesList\Controller;
 
  use Zend\Mvc\Controller\AbstractActionController;
- use Zend\View\Model\ViewModel;
 
  class PlacesListController extends AbstractActionController
  {
-     public function indexAction()
-     {
-     }
-
-     public function addAction()
-     {
-     }
-
-     public function editAction()
-     {
-     }
-
-     public function deleteAction()
-     {
-     }
+    
  }
  
 ?>

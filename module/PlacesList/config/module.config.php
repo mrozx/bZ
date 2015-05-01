8b6a77c6f687396926a11e3cb3b020c5ea40db8c
Maoz <maozx2@gmail.com>
2015-05-01 18:35:42 +0200

    testF

--- module/PlacesList/config/module.config.php
+++ module/PlacesList/config/module.config.php
@@ -21,7 +21,7 @@ return array(
 	'service_manager' => array(
          'factories' => array(
 			 'PlacesList\Mapper\PostMapperInterface'   => 'PlacesList\Factory\ZendDbSqlMapperFactory',
-             'PlacesList\Service\PlacesListServiceInterface' => 'PlacesList\Factory\PlacesListServiceFactory'
+             'PlacesList\Service\PlacesListServiceInterface' => 'PlacesList\Factory\PlacesListServiceFactory',
 			 'Zend\Db\Adapter\Adapter'           => 'Zend\Db\Adapter\AdapterServiceFactory'
          )
      ),

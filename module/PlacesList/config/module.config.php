417788f7d47577b904ea8e560b2922b6419cc6b1
Maoz <maozx2@gmail.com>
2015-05-01 20:40:16 +0200

    withPlaceList05

--- module/PlacesList/config/module.config.php
+++ module/PlacesList/config/module.config.php
@@ -37,7 +37,7 @@ return array(
 		 // Open configuration for all possible routes
          'routes' => array(
 			// Define a new route called "placesList"
-             'placesList' => array(
+             'posts' => array(
 				// Define the routes type. The segment route allows us to specify placeholders in the URL pattern (route) that will be mapped to named parameters in the matched route.
                  'type'    => 'literal',
 				 // Configure the route itself
@@ -45,20 +45,11 @@ return array(
 					// the route is ``/album[/:action][/:id]`` which will match any URL that starts with /album. 
 					//The next segment will be an optional action name, 
 					//and then finally the next segment will be mapped to an optional id. 
-                    /*  'route'    => '/placesList[/:action][/:id]',
-                     'constraints' => array(
-                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                         'id'     => '[0-9]+',
-                     ), */
 					  'route'    => '/blog',
                      // Define default controller and action to be called when this route is matched
                      'defaults' => array(
                          'controller' => 'PlacesList\Controller\PlacesList',
                          'action'     => 'index',
-					 // Define default controller and action to be called when this route is matched
-                    /*  'defaults' => array(
-                         'controller' => 'PlacesList\Controller\PlacesList',
-                         'action'     => 'index', */
                      ),
                  ),
              ),

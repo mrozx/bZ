bb2a1c7669be6418a936d2ae9836fc4f5b118ef9
Maoz <maozx2@gmail.com>
2015-05-01 23:58:07 +0200

    withPlaceList05

--- module/PlacesList/config/module.config.php
+++ module/PlacesList/config/module.config.php
@@ -58,7 +58,9 @@ return array(
 	 
      'view_manager' => array(
          'template_path_stack' => array(
-				__DIR__ . '/view',
+		  'placesList' => array(
+				__DIR__ . '/../view',
+				),
          ),
      ),
 	 

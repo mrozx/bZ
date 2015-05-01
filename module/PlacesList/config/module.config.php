a9e984f37b7dbcacdb1f01df6a4ec4e2695d9a75
Maoz <maozx2@gmail.com>
2015-05-01 18:23:09 +0200

    difRouth

--- module/PlacesList/config/module.config.php
+++ module/PlacesList/config/module.config.php
@@ -46,15 +46,20 @@ return array(
 					// the route is ``/album[/:action][/:id]`` which will match any URL that starts with /album. 
 					//The next segment will be an optional action name, 
 					//and then finally the next segment will be mapped to an optional id. 
-                     'route'    => '/placesList[/:action][/:id]',
+                    /*  'route'    => '/placesList[/:action][/:id]',
                      'constraints' => array(
                          'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                          'id'     => '[0-9]+',
-                     ),
-					 // Define default controller and action to be called when this route is matched
+                     ), */
+					  'route'    => '/blog',
+                     // Define default controller and action to be called when this route is matched
                      'defaults' => array(
                          'controller' => 'PlacesList\Controller\PlacesList',
                          'action'     => 'index',
+					 // Define default controller and action to be called when this route is matched
+                    /*  'defaults' => array(
+                         'controller' => 'PlacesList\Controller\PlacesList',
+                         'action'     => 'index', */
                      ),
                  ),
              ),

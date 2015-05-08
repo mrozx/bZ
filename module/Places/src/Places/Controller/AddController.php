891b9641ec221be0c6646f777057ab402888c03b
Maoz <maozx2@gmail.com>
2015-05-08 13:46:56 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -3,8 +3,8 @@
 namespace Places\Controller;
 
  use Places\Service\PlacesServiceInterface;
-  use Places\Controller\AddoneController;
-    use Places\Form\PlacesAddSubOneForm;
+ use Places\Controller\AddoneController;
+ use Places\Form\PlacesAddSubOneForm;
  use Zend\Mvc\Controller\AbstractActionController;
  use Zend\View\Model\ViewModel;
 
@@ -22,10 +22,7 @@ namespace Places\Controller;
 		
         $id = $this->params()->fromRoute('step');
 		  if($id == 1) {
-			  // $primaryView = new ViewModel(array(
-             // 'form' => "dsds"
-			// ));
-		// $primaryView->setTemplate('write/add');
+			
 			$one = new PlacesAddSubOneForm(null,null);
 			$formOne = new AddoneController($this->placesService, $one);
 			return $formOne->addAction();

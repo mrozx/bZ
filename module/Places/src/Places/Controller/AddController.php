c19b86743013a1620a7c430203cde6f664410754
Maoz <maozx2@gmail.com>
2015-05-08 22:55:10 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -16,7 +16,7 @@ namespace Places\Controller;
     protected $placesService;
 	protected $placesFormOne;
 	protected $placesFormTwo;
-	protected $formStep = 1;
+	protected static $formStep = 1;
 	
      public function __construct(
 		PlacesServiceInterface $placesService,

99972aeecb9c2cfffdd1c2019c154d0a14011438
Maoz <maozx2@gmail.com>
2015-05-08 13:39:23 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -4,6 +4,7 @@ namespace Places\Controller;
 
  use Places\Service\PlacesServiceInterface;
   use Places\Controller\AddoneController;
+    use Places\Form\PlacesAddSubOneForm;
  use Zend\Mvc\Controller\AbstractActionController;
  use Zend\View\Model\ViewModel;
 
@@ -25,7 +26,8 @@ namespace Places\Controller;
              // 'form' => "dsds"
 			// ));
 		// $primaryView->setTemplate('write/add');
-		$formOne = new AddoneController($this->placesService, null);
+		$one = new PlacesAddSubOneForm();
+		$formOne = new AddoneController($this->placesService, $one);
 			return $formOne->addAction();
 			}
      }

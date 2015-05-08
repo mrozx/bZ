d59b5e17ef046d5dc56d8595cea2b0b76c8a0cac
Maoz <maozx2@gmail.com>
2015-05-08 13:42:51 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -28,7 +28,7 @@ namespace Places\Controller;
 		// $primaryView->setTemplate('write/add');
 		$one = new PlacesAddSubOneForm(null,null);
 		$formOne = new AddoneController($this->placesService, $one);
-			return null;
+			return $formOne->add();
 			}
      }
  }

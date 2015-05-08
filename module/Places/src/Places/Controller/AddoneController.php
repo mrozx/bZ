df7c89666f29cbbfd3d4ccf091b9c2251d613062
Maoz <maozx2@gmail.com>
2015-05-08 15:03:08 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddoneController.php
+++ module/Places/src/Places/Controller/AddoneController.php
@@ -45,7 +45,7 @@ namespace Places\Controller;
                   //   $this->placesService->savePlace($this->placesForm->getData());
 
                    
-					return $this->redirect()->toRoute('add/2');
+					return $this->redirect()->toRoute('places');
                  } catch (\Exception $e) {
                      // Some DB Error happened, log it and let the user know
                  }

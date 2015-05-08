beb95a06e75756c1332a565f6a9330f6cd8dfdce
Maoz <maozx2@gmail.com>
2015-05-08 12:15:30 +0200

    withSubForm

--- module/Places/config/module.config.php
+++ module/Places/config/module.config.php
@@ -78,7 +78,7 @@ return array(
 	),
      'view_manager' => array(
          'template_path_stack' => array(
-				__DIR__ . '/../view/',
+			'places' =>	__DIR__ . '/../view/',
          ),
      ),
 	 

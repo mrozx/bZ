0c22da1039333d4c4bd364794f40a6358b4e1f2f
Maoz <maozx2@gmail.com>
2015-05-08 15:22:16 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddoneController.php
+++ module/Places/src/Places/Controller/AddoneController.php
@@ -29,17 +29,17 @@ namespace Places\Controller;
          if ($request->isPost()) {
              $this->placesForm->setData($request->getPost());
 
-             if ($this->placesForm->isValid()) {
+             //if ($this->placesForm->isValid()) {
                  try {
 					\Zend\Debug\Debug::dump($this->placesForm->getData());die();
                   //   $this->placesService->savePlace($this->placesForm->getData());
 
                    
-					return $this->redirect()->toRoute('add/2');
+				//	return $this->redirect()->toRoute('add/2');
                  } catch (\Exception $e) {
                      // Some DB Error happened, log it and let the user know
                  }
-             }
+             //}
          }
 		 
 

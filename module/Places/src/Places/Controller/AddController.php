96b0fa4044be1e143b9d31a8b425cfe61a08b5b7
Maoz <maozx2@gmail.com>
2015-05-09 17:23:47 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -69,6 +69,8 @@ namespace Places\Controller;
 				// \Zend\Debug\Debug::dump($this->placesFormOne->getData());die();
                   //   $this->placesService->savePlace($this->placesFormOne->getData());
 				    $form_session->step = 3;
+					$form_session->dataArray = $this->placesFormOne->getData();
+					\Zend\Debug\Debug::dump($form_session->dataArray);die();
 				    $primaryView = new ViewModel(array(
                     'form' => $this->placesFormTwo,
 					'nextstep' => $form_session->step 
@@ -87,6 +89,8 @@ namespace Places\Controller;
 			// $formOne = new AddoneController($this->placesService, $one);
 			// return $formOne->addAction();
 			$form_session->step = 1;
+			$form_session->dataArray = new array();
+			
 			 $primaryView = new ViewModel(array(
              'form' => $this->placesFormOne,
 			 'nextstep' => $form_session->step
@@ -97,33 +101,6 @@ namespace Places\Controller;
 			return $primaryView;
 		}	
 			
-	public function loadform3Action() {	
-	\Zend\Debug\Debug::dump("dsas");die();
-			$request = $this->getRequest();
-			if ($request->isPost()) {
-				 $this->placesFormTwo->setData($request->getPost());
-
-				 if ($this->placesFormTwo->isValid()) {
-					 try {
-						\Zend\Debug\Debug::dump($this->placesFormTwo->getData());die();
-					  //   $this->placesService->savePlace($this->placesFormTwo->getData());
-					   $this->formStep = 3;
-						$primaryView = new ViewModel(array(
-						'nextstep' => $this->formStep + 1
-						//'test' => $this->placesService->getName()
-						));
-				
-						$primaryView->setTemplate('write/add');
-						return $primaryView;
-					 } catch (\Exception $e) {
-						 // Some DB Error happened, log it and let the user know
-					 }
-				 }
-			 }
-			
-		
-
-     }
  }
  
 ?>

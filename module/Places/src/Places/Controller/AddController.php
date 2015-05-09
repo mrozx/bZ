f051786c45a838b490b4c25381985dde33779d9e
Maoz <maozx2@gmail.com>
2015-05-09 22:25:05 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -94,7 +94,7 @@ namespace Places\Controller;
 			// return $formOne->addAction();
 			$form_session->step = 1;
 			$form_session->dataArray = array();
-			$form_session->obj = new PlacesService();
+			$form_session->obj = $this->placesService();
 			 $primaryView = new ViewModel(array(
              'form' => $this->placesFormOne,
 			 'nextstep' => $form_session->step

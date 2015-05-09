c39cbaaec58960b5b3f7254360c8463656506955
Maoz <maozx2@gmail.com>
2015-05-09 17:41:20 +0200

    withSubForm

--- module/Places/src/Places/Controller/AddController.php
+++ module/Places/src/Places/Controller/AddController.php
@@ -71,12 +71,13 @@ namespace Places\Controller;
 				// \Zend\Debug\Debug::dump($this->placesFormOne->getData());die();
                   //   $this->placesService->savePlace($this->placesFormOne->getData());
 				    $form_session->step = 3;
-					$form_session->dataArray =  array_merge($form_session->dataArray, $this->placesFormTwo->getData());;
 					\Zend\Debug\Debug::dump($form_session->dataArray);die();
+					$form_session->dataArray =  array_merge($form_session->dataArray, $this->placesFormTwo->getData());;
+					
 					
 				    $primaryView = new ViewModel(array(
-                    'form' => $this->placesFormTwo,
-					'nextstep' => $form_session->step 
+						'form' => $this->placesFormTwo,
+						'nextstep' => $form_session->step 
 					));
 				   $primaryView->setTemplate('write/add');
 				   return $primaryView;
